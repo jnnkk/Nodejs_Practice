79b46312b2ab6cb2315409d07ccd0791b9a59d06
SJY <jun000405@naver.com>
2023-11-09 18:15:04 +0900

    fix: 주석 추가

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set('view engine', 'pug'); // pug 를 사용하겠다는 의미
+app.set('views', 'views'); // views 라는 폴더를 views로 사용하겠다는 의미
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found' }); // 옵션을 객체로 줄 수 있음
 });
 
 app.listen(3000);
